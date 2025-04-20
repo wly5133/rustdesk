631f720411e71ec46ff86d867957231c61b980cd
WLY5133 <weiloyang5@163.com>
2025-04-20 21:10:24 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -940,7 +940,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://144.24.75.195".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
